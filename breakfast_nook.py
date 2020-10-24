1db303930ff575cd7c4e75dc579340c2260d1846
AudreyA20 <71663081+AudreyA20@users.noreply.github.com>
2020-10-24 17:56:26 -0400

    Add files via upload

--- breakfast_nook.py
+++ breakfast_nook.py
@@ -50,20 +50,11 @@ class breakfast_nook:
         self.air.actuators.append(actuator("Breakfast_Nook Air Actuator","Air",dbCursor))
 
         #-------------------------------------------------------------
-        #   CAMERAS
+        #   SMOKE DETECTOR
         #-------------------------------------------------------------
 
-        self.cameras = device("Breakfast_Nook Camera",dbCursor)
-        self.cameras.append(device("Breakfast_Nook Camera",dbCursor))
-        self.cameras.actuators.append(actuator("Breakfast_Nook Camera Actuator","Breakfast_Nook Camera",dbCursor))
-        self.cameras.sensors.append(motionSensor("BNCMS","Breakfast_Nook Camera",dbCursor))
-
-        #-------------------------------------------------------------
-        #   SMOKE ALARM
-        #-------------------------------------------------------------
-
-        self.smokealarm = device("Breakfast_Nook Alarm", dbCursor)
-        self.smokealarm.sensor.append(SmokeSensor("BNSS","Breakfast_Nook Alarm", dbCursor))
+        self.smokedetector = device("Breakfast_Nook Detector", dbCursor)
+        self.smokedetector.sensor.append(SmokeSensor("BNSS","Breakfast_Nook Detector", dbCursor))
 
 
     #-------------------------------------------------------------
@@ -71,6 +62,27 @@ class breakfast_nook:
     #-------------------------------------------------------------
     #   LIGHTS
     #-------------------------------------------------------------
+
+    def turnOnLights(self):
+        self.lights.actuators[0].turnOn()
+    
+    def turnOffLights(self):
+        self.lights.actuators[0].turnOff()
+    
+    def getLightsState(self):
+        return self.lights.actuators[0].getState()
+
+    def setLightBrightness(self,bright):
+        self.lights.sensors[0].setBrightPct(bright)
+
+    def getLightBrightness(self):
+        return self.lights.sensors[0].getBrightPct()
+
+    def setLightsMotion(self, value):
+        self.lights.sensors[1].updateIsMotion(value)
+
+    def getLightsMotion(self):
+        return self.lights.sensors[1].getMotion()
 
     #-------------------------------------------------------------
     #   DOORS
@@ -82,14 +94,93 @@ class breakfast_nook:
     #   WINDOWS
     #-------------------------------------------------------------
 
+    def openWindow(self,winNum):
+        self.windows[winNum].actuators[0].turnOn()
+        self.windows[winNum].sensors[0].updateOpen()
+
+    def closeWindow(self,winNum):
+        self.windows[winNum].actuators[0].turnOff()
+        self.windows[winNum].sensors[0].updateClosed()
+
+    def getWindowState(self,winNum):
+        return self.windows[winNum].actuators[0].getState()
+
+    def getWindowOpenCloseState(self, winNum):
+        return self.windows[winNum].sensors[0].getState()
+
     #-------------------------------------------------------------
     #   AC/HEAT
     #-------------------------------------------------------------
 
-    #-------------------------------------------------------------
-    #   CAMERAS
-    #-------------------------------------------------------------
+    #AC
+    def turnOnAC(self):
+        self.aircon.actuators[0].turnOn()
+
+    def turnOffAC(self):
+        self.aircon.actuators[0].turnOff()
+
+    def getACState(self):
+        return self.aircon.actuators[0].getState()
+
+    def setACTemp(self,temp):
+        self.aircon.sensors[0].setTemp(temp)
+
+    def getACTemp(self):
+        return self.aircon.sensors[0].getTemp()
+
+    def updateACTemp(self):
+        currTemp = self.getTemp()
+        newTemp = currTemp + self.ACDELTAT
+        self.setACTemp(newTemp)
+        self.setHeatTemp(newTemp)
+
+    #HEAT
+    def turnOnHeat(self):
+        self.heat.actuators[0].turnOn()
+
+    def turnOffHeat(self):
+        self.heat.actuators[0].turnOff()
+
+    def getHeatState(self):
+        return self.heat.actuators[0].getState()
+
+    def setHeatTemp(self, temp):
+        self.heat.sensors[0].setTemp(temp)
+
+    def getHeatTemp(self):
+        return self.heat.sensors[0].getTemp()
+
+    def updateHeatTemp(self):
+        currTemp = self.getTemp()
+        newTemp = currTemp + self.HEATDELTAT
+        self.setACTemp(newTemp)
+        self.setHeatTemp(newTemp)
+
+    #Shared
+    def getTemp(self):
+        if self.getACTemp() == self.getHeatTemp():
+            return self.getACTemp()
+        else:
+            print("error temp sensors missmatched (should never be here)")
 
     #-------------------------------------------------------------
-    #   SMOKE ALARM
+    #   SMOKE Detector
     #-------------------------------------------------------------
+
+    def turnOnSmokeDetector(self):
+        self.smokeDetector.actuators[0].turnOn()
+
+    def turnOffSmokeDetector(self):
+        self.smokeDetector.actuators[0].turnOff()
+
+    def getSmokeDetectorState(self):
+        return self.smokeDetector.actuators[0].getState()
+
+    def setSmokeState(self, isSmoke):
+        if isSmoke == 1:
+            self.smokeDetector.sensors[0].updateOpen()
+        else:
+            self.smokeDetector.sensors[0].updateClosed()
+    
+    def getSmokeState(self):
+        return self.smokeDetector.sensors[0].getState()
